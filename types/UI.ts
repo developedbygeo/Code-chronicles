6e4acd535a018ac9326a985ff44d97ac07e6aadc
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-21 02:58:30 +0100

    Add UI types

---
+++ types/UI.ts
@@ -0,0 +1,13 @@
+export type WithClassName = {
+    className?: string;
+};
+
+export type WithChildren = {
+    children?: React.ReactNode;
+};
+
+export type WithSource = {
+    src: string;
+};
+
+export type AnimationDirection = 'up' | 'down' | 'left' | 'right' | 'default';
