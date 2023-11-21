b2d2bc269535a5808cc99292d6f8b2348cad967e
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-21 03:09:06 +0100

    Update Sheet component styles

--- components/ui/Sheet.tsx
+++ components/ui/Sheet.tsx
@@ -23,7 +23,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
     <SheetPrimitive.Overlay
         className={cn(
-            'fixed inset-0 z-50 bg-gradient-background backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+            'fixed inset-0 z-50 border-0 bg-gray-100 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 dark:bg-dark',
             className
         )}
         {...props}
@@ -34,7 +34,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-    'fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+    'fixed z-50 gap-4 bg-white p-6 transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
     {
         variants: {
             side: {
@@ -66,7 +66,7 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
                 {children}
                 <SheetPrimitive.Close
                     className={cn(
-                        'absolute right-4 top-4 rounded-sm opacity-70 transition-opacity data-[state=open]:bg-gradient-background hover:opacity-100 focus:outline-none disabled:pointer-events-none',
+                        'absolute right-4 top-5 rounded-sm opacity-70 transition-opacity data-[state=open]:bg-gradient-background hover:opacity-100 focus:outline-none disabled:pointer-events-none',
                         buttonClassName
                     )}
                 >
