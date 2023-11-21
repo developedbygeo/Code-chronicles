3984adda247afb0ffa0d6abd5af9bfb4083661a6
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-21 02:57:21 +0100

    Add Prettier and ESLint configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,96 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: 0 },
-          to: { height: "var(--radix-accordion-content-height)" },
+    future: { hoverOnlyWhenSupported: true },
+    darkMode: ['class'],
+    content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+        extend: {
+            height: {
+                '10/12': '83.333333%',
+                '11/12': '91.666667%',
+            },
+            colors: {
+                accent: '#7987FF',
+                dark: '#2D2D30',
+            },
+            textShadow: {
+                '2xl': '1px 1px 5px rgb(33 34 43 / 20%)',
+                '3xl': '0 0 3px rgba(0, 0, 0, .8), 0 0 5px rgba(0, 0, 0, .9)',
+                'hero-header': '1px 1px 5px rgba(121,135,255,1)',
+            },
+            backgroundImage: {
+                'gradient-background': 'linear-gradient(to top, #191B1F, #26292E)',
+                'gradient-container': 'linear-gradient(to bottom, #26292E, #191B1F)',
+            },
+            boxShadow: {
+                'container-neumorph':
+                    '-0.5rem -0.625rem 2.5rem -0.3125rem rgba(150, 190, 231, 0.15), 0.5rem 0.625rem 2.5rem 0.3125rem rgba(10, 10, 10, 0.7)',
+                'container-neumorph-sm':
+                    '-0.5rem -0.625rem 2.5rem -0.3125rem rgba(150, 190, 231, 0.1), 0.5rem 0.625rem 2.5rem 0.3125rem rgba(10, 10, 10, 0.3)',
+            },
+            fontFamily: {
+                sans: ['var(--font-montserrat)', ...defaultTheme.fontFamily.sans],
+                anton: ['var(--font-anton)', ...defaultTheme.fontFamily.sans],
+            },
+            keyframes: {
+                spinning: {
+                    from: { transform: 'rotate(0deg)' },
+                    to: { transform: 'rotate(360deg)' },
+                },
+                'accordion-down': {
+                    from: { height: 0 },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: 0 },
+                },
+                'bubble-bounce': {
+                    '0%, 100%': {
+                        transform: 'translateY(-1.5rem)',
+                    },
+                    '50%': {
+                        transform: 'translateY(1.5rem)',
+                    },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+                bubble: 'bubble-bounce 8s ease-in-out infinite alternate',
+                'bubble-mid': 'bubble-bounce 12s ease-in-out infinite alternate',
+                'bubble-slow': 'bubble-bounce 22s linear infinite',
+                spinning: 'spinning 4s linear infinite',
+            },
         },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+    plugins: [
+        require('tailwindcss-animate'),
+        require('tailwindcss-fluid-type')({
+            values: {
+                xs: [-2, 1.6],
+                sm: [-1, 1.6],
+                base: [0, 1.6],
+                lg: [1, 1.6],
+                xl: [2, 1.2],
+                '2xl': [3, 1.2],
+                '3xl': [4],
+                '4xl': [5],
+                '5xl': [6, 1.1],
+                '6xl': [7, 1.1],
+                '7xl': [8, 1],
+                '8xl': [9, 1],
+                '9xl': [10, 1],
+            },
+        }),
+        require('tailwind-scrollbar')({ nocompatible: true }),
+        require('tailwindcss-textshadow'),
+    ],
+};
