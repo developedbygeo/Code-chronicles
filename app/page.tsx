190185377f491f5f3c1c829f234b3ebdb8d339fa
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-23 01:50:44 +0100

    Add RecentPosts and MostPopular components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import Image from 'next/image';
-
 import Featured from '@/modules/Home/Featured';
 import BlogCategories from '@/modules/Home/BlogCategories';
+import RecentPosts from '@/modules/Home/RecentPosts';
+import MostPopular from '@/modules/Home/MostPopular';
 
 export default function Home() {
     return (
         <section>
             <Featured className="apply-pt" />
             <BlogCategories className="apply-pt" />
+            <div className="apply-pt grid grid-cols-1 xl:grid-cols-12">
+                <RecentPosts className="xl:col-span-8" />
+                <MostPopular className="xl:col-span-2 xl:col-start-11" />
+            </div>
         </section>
     );
 }
