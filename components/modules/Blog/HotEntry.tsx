3e0eda5c41c8ade900d88bf0b80a2e715d0a7ba9
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-23 01:51:11 +0100

    Add HotEntry component

---
+++ components/modules/Blog/HotEntry.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { LuDot } from 'react-icons/lu';
+
+import { cn } from '@/lib/utils';
+
+import { WithClassName } from '@/types/UI';
+import { PopularEntryPreview } from '@/types/blogTypes';
+
+import CategoryBadge from '@/elements/CategoryBadge';
+
+type PopularEntryProps = WithClassName & {
+    entry: PopularEntryPreview;
+};
+
+const HotEntry = ({ className, entry }: PopularEntryProps) => (
+    <article className={cn('mb-12', className)}>
+        <div className="group mb-2 flex flex-col-reverse gap-2">
+            <Link className="peer" href={entry.slug}>
+                <h3 className="themed-text text-base font-normal tracking-tighter  hover:text-gray-800 dark:hover:text-gray-100">
+                    {entry.title}
+                </h3>
+            </Link>
+            <CategoryBadge className="w-fit peer-hover:scale-100 peer-hover:shadow-lg" variant={entry.category}>
+                {entry.category}
+            </CategoryBadge>
+        </div>
+        <div className="flex justify-start gap-2 text-xs">
+            <div className="flex items-center gap-2">
+                <span className="themed-text font-normal">{entry.author}</span>
+                <LuDot className="h-4 w-4 text-dark" />
+            </div>
+            <span className="themed-minor-text font-light ">{entry.date}</span>
+        </div>
+    </article>
+);
+
+export default HotEntry;
