def1136b692b3e46928ee79e2c193d750ec3af75
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-24 02:09:14 +0100

    Refactor MostPopular component to use section instead of aside

--- components/modules/Home/MostPopular.tsx
+++ components/modules/Home/MostPopular.tsx
@@ -6,13 +6,13 @@ import HotEntry from '@/modules/Blog/HotEntry';
 import { PopularEntryPreview } from '@/types/blogTypes';
 
 const MostPopular = ({ className }: WithClassName) => (
-    <aside className={cn('', className)}>
+    <section className={cn('', className)}>
         <p className="text-sm text-gray-500 dark:text-gray-400">What&apos;s hot</p>
         <h2 className="section-title">Most Popular</h2>
         {mockData.map((entry) => (
             <HotEntry key={entry.id} entry={entry as PopularEntryPreview} />
         ))}
-    </aside>
+    </section>
 );
 
 export default MostPopular;
