11ab28dc11ddafc98969b01698a32d78a1e02a37
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-24 02:09:36 +0100

    Refactor Home component to include new modules

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,24 @@ import Featured from '@/modules/Home/Featured';
 import BlogCategories from '@/modules/Home/BlogCategories';
 import RecentPosts from '@/modules/Home/RecentPosts';
 import MostPopular from '@/modules/Home/MostPopular';
+import SimpleBlogCategories from '@/modules/Blog/SimpleBlogCategories';
+import EditorPicks from '@/modules/Home/EditorPicks';
 
-export default function Home() {
+const Home = () => {
     return (
         <section>
             <Featured className="apply-pt" />
             <BlogCategories className="apply-pt" />
             <div className="apply-pt grid grid-cols-1 xl:grid-cols-12">
                 <RecentPosts className="xl:col-span-8" />
-                <MostPopular className="xl:col-span-2 xl:col-start-11" />
+                <aside className="flex flex-col gap-12 xl:col-span-2 xl:col-start-11">
+                    <MostPopular />
+                    <SimpleBlogCategories listClassName="grid grid-cols-2 gap-6" />
+                    <EditorPicks className="mt-6" />
+                </aside>
             </div>
         </section>
     );
-}
+};
+
+export default Home;
