f5a95b24c9d091b4f40b3a4c73a970cb0000a0d0
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-26 02:52:06 +0100

    Add social media icons and links

---
+++ data/socialMedia.tsx
@@ -0,0 +1,32 @@
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+
+const socialMedia = [
+    {
+        name: 'Github',
+        href: 'https://github.com/developedbygeo',
+        icon: FaGithub,
+    },
+    {
+        name: 'Facebook',
+        href: 'https://www.facebook.com',
+        icon: FaFacebook,
+    },
+    {
+        name: 'Instagram',
+        href: 'https://www.instagram.com',
+        icon: FaInstagram,
+    },
+    {
+        name: 'Twitter',
+        href: 'https://twitter.com',
+        icon: FaTwitter,
+    },
+
+    {
+        name: 'YouTube',
+        href: 'https://www.youtube.com',
+        icon: FaYoutube,
+    },
+];
+
+export default socialMedia;
