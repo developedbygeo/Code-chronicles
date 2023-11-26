b450fbd41f7ef071a296398f2e7277c36250243a
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-27 00:23:12 +0100

    Refactor Footer component and update social media links

--- components/modules/Footer.tsx
+++ components/modules/Footer.tsx
@@ -1,9 +1,11 @@
-import socialMedia from '@/data/socialMedia';
-import Logo from '@/assets/default-monochrome.svg';
-import blogCategories from '@/data/blogCategories';
 import Link from 'next/link';
-import { WithClassName } from '@/types/UI';
+
 import { cn } from '@/lib/utils';
+import { WithClassName } from '@/types/UI';
+
+import Logo from '@/assets/default-monochrome.svg';
+import socialMedia from '@/data/socialMedia';
+import blogCategories from '@/data/blogCategories';
 
 const Footer = ({ className }: WithClassName) => {
     return (
@@ -14,7 +16,7 @@ const Footer = ({ className }: WithClassName) => {
             <h2 id="footer-heading" className="sr-only">
                 Footer
             </h2>
-            <div className="container flex">
+            <div className="container flex flex-col xl:flex-row">
                 <div className="flex flex-1 flex-col space-y-8">
                     <Logo className="h-auto w-56 fill-black transition-colors dark:fill-gray-200 lg:w-48" />
                     <p className="themed-minor-text">Designing & developing apps, websites and systems for the web.</p>
@@ -23,22 +25,38 @@ const Footer = ({ className }: WithClassName) => {
                             const Icon = item.icon;
 
                             return (
-                                <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+                                <a
+                                    key={item.name}
+                                    href={item.href}
+                                    target="_blank"
+                                    referrerPolicy="no-referrer"
+                                    className="text-gray-400 hover:text-gray-500"
+                                >
                                     <span className="sr-only">{item.name}</span>
                                     <Icon className="h-6 w-6 transition-colors hover:text-dark dark:text-gray-400 dark:hover:text-gray-100" />
                                 </a>
                             );
                         })}
                     </ul>
+                    <div className="flex flex-1 flex-col space-y-8 xl:hidden">
+                        <h3 className="themed-minor-text text-sm font-semibold">Categories</h3>
+                        <ul className="mt-6 flex flex-col gap-4 xl:flex-row xl:items-center">
+                            {blogCategories.map((category) => (
+                                <Link key={category.name} href={`/blog?category=${category.slug}`} className="">
+                                    <span className="navlink">{category.name}</span>
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
                     <div className="mt-4 pt-8">
                         <p className="themed-minor-text text-xs font-light">
                             &copy; {new Date().getFullYear()} developedbygeo. All rights reserved.
                         </p>
                     </div>
                 </div>
-                <div className="flex flex-1 flex-col space-y-8">
+                <div className="hidden flex-1 flex-col space-y-8 xl:flex">
                     <h3 className="themed-minor-text text-sm font-semibold">Categories</h3>
-                    <ul className="mt-6 flex items-center gap-4">
+                    <ul className="mt-6 flex flex-col gap-4 xl:flex-row xl:items-center">
                         {blogCategories.map((category) => (
                             <Link key={category.name} href={`/blog?category=${category.slug}`} className="">
                                 <span className="navlink font-light">{category.name}</span>
