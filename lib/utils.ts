d36fd8c1e8e9594f40e7e6cf0e1b709983b3c1cf
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-27 01:47:52 +0100

    Refactor utils.ts: Add parseDate function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { parseISO, format } from 'date-fns';
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const parseDate = (dateString: string) => format(parseISO(dateString), 'LLLL d, yyyy');
