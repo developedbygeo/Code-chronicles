814472e06f1bc715ac3592d7a282420e2a054da6
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-27 01:47:29 +0100

    Refactor module imports and add HeroTitle component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
-import Featured from '@/modules/Home/Featured';
-import BlogCategories from '@/modules/Home/BlogCategories';
-import RecentPosts from '@/modules/Home/RecentPosts';
-import MostPopular from '@/modules/Home/MostPopular';
-import SimpleBlogCategories from '@/modules/Posts/SimpleBlogCategories';
-import EditorPicks from '@/modules/Home/EditorPicks';
+import Featured from '@/modules/Posts/Featured';
+import BlogCategories from '@/modules/Posts/BlogCategories';
+import RecentPosts from '@/modules/Posts/RecentPosts';
+import MostPopular from '@/modules/Posts/MostPopular';
+import SimpleBlogCategories from '@/modules/PostModules/SimpleBlogCategories';
+import EditorPicks from '@/modules/Posts/EditorPicks';
+import HeroTitle from '@/modules/UI/HeroTitle';
+
+import UnderlineGraphic from '@/assets/underline-1.svg';
 
 const Home = () => {
     return (
         <section>
-            <Featured className="apply-pt" />
+            <section className="apply-pt">
+                <HeroTitle>
+                    <div className="mb-3">
+                        Hello, I am{' '}
+                        <div className="relative inline overflow-visible">
+                            <span className="animated-gradient-text relative z-20 fill-gray-100 dark:fill-dark">
+                                George{' '}
+                            </span>
+                            <UnderlineGraphic className="animated-gradient-text absolute -bottom-6 left-0 h-12 w-full" />
+                        </div>
+                        ! 👋
+                    </div>
+                    Find out more about frontend development, creative ideas and everything in between!
+                </HeroTitle>
+            </section>
+            <Featured className="" />
             <BlogCategories className="apply-pt" />
             <div className="apply-pt grid grid-cols-1 xl:grid-cols-12">
                 <RecentPosts className="xl:col-span-8" />
