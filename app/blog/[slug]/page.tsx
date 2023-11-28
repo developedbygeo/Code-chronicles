d12fc3e1fef1db463e2f27e8cfa42980f1118956
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-28 08:05:37 +0100

    Refactor blog post page layout and components

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,7 +4,10 @@ import Image from 'next/image';
 
 import post from '@/data/blog-post.json';
 import PostHeader from '@/modules/Post/PostHeader';
-import PostComments from '@/modules/Post/PostComments';
+import PostCommentSection from '@/modules/Post/PostCommentSection';
+import MostPopular from '@/modules/Posts/MostPopular';
+import EditorPicks from '@/modules/Posts/EditorPicks';
+import { BlogPost } from '@/types/blogTypes';
 
 type BlogPostParams = {
     params: {
@@ -18,12 +21,18 @@ const BlogPost = async ({ params }: BlogPostParams) => {
     console.log(slug);
 
     return (
-        <section className="apply-pt">
-            <PostHeader author={post.author} title={post.title} image={post.image} publishedAt={post.publishedAt} />
-            <div className="mt-12">
-                <p className="themed-blog-text">{post.text}</p>
+        <section className="apply-py">
+            <PostHeader post={post as BlogPost} />
+            <div className="grid grid-cols-12">
+                <div className="xl:col-span-9">
+                    <p className="themed-blog-text mt-12">{post.text}</p>
+                    <PostCommentSection className="apply-pt xl:col-span-8" />
+                </div>
+                <div className="mt-12 xl:col-span-3 xl:col-start-11">
+                    <MostPopular className="xl:col-span-3 xl:col-start-9" />
+                    <EditorPicks className="apply-pt xl:col-span-3 xl:col-start-9" />
+                </div>
             </div>
-            <PostComments />
         </section>
     );
 };
