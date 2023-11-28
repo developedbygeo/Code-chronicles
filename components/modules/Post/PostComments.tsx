294d1944ade4ac5c11a6f10ab6b4f54d2c0b0897
developedbygeo <georgiosanagnostou@hotmail.com>
2023-11-28 08:05:57 +0100

    Refactor PostComments component to display comments

--- components/modules/Post/PostComments.tsx
+++ components/modules/Post/PostComments.tsx
@@ -1,16 +1,22 @@
-import Link from 'next/link';
-
+import { cn } from '@/lib/utils';
+import PostAuthor from '@/modules/Post/PostAuthor';
 import { WithClassName } from '@/types/UI';
-import CommentForm from '@/modules/Post/CommentForm';
+import { Comment } from '@/types/comments';
 
-const PostComments = ({ className }: WithClassName) => {
-    const isAuth = true;
+type PostCommentsProps = WithClassName & {
+    comments: Comment[];
+};
 
+const PostComments = ({ className, comments }: PostCommentsProps) => {
     return (
-        <section className="container">
-            <h2>Comments</h2>
-            {isAuth ? <CommentForm /> : <Link href="/login">Sign in to comment</Link>}
-        </section>
+        <ul className={cn('flex flex-col gap-1', className)}>
+            {comments.map((comment) => (
+                <li key={comment.id}>
+                    <PostAuthor author={comment.author} date={comment.date} />
+                    <p className="themed-minor-text mt-2">{comment.text}</p>
+                </li>
+            ))}
+        </ul>
     );
 };
 
