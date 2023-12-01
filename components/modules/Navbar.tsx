84337f22d94c3a02308a6074eead6be6091cd6e8
developedbygeo <georgiosanagnostou@hotmail.com>
2023-12-01 01:30:21 +0100

    Add MobileMenu component to Navbar

--- components/modules/Navbar.tsx
+++ components/modules/Navbar.tsx
@@ -3,21 +3,26 @@ import { WithClassName } from '@/types/UI';
 
 import AuthenticatedLinks from '@/modules/AuthenticatedLinks';
 import ThemeToggle from '@/modules/ThemeToggle';
+import MobileMenu from '@/modules/MobileMenu';
 
+import { cn } from '@/lib/utils';
 import NavLink from '@/ui/NavLink';
 
 import Logo from '@/assets/default-monochrome.svg';
 
 import navLinks from '@/data/navigation.json';
-import MobileMenu from '@/modules/MobileMenu';
 
-const Navbar = ({ className }: WithClassName) => (
-    <div className="flex justify-between py-8 xl:px-4">
+type NavbarProps = WithClassName & {
+    shouldHideThemeToggle?: boolean;
+};
+
+const Navbar = ({ className, shouldHideThemeToggle }: NavbarProps) => (
+    <div className={cn('flex justify-between py-8 xl:px-4', className)}>
         <Link href="/">
             <Logo className="h-auto w-56 fill-black transition-colors dark:fill-gray-200 lg:w-72" />
         </Link>
         <nav className="hidden items-center xl:flex">
-            <ThemeToggle className="mr-12" />
+            {!shouldHideThemeToggle && <ThemeToggle className="mr-12" />}
 
             <ul className="flex justify-center space-x-12 text-base">
                 {navLinks.map((link) => (
