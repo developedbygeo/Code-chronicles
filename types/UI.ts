6f9d10617360007d5165364bb22f0aa5f5d62eae
developedbygeo <georgiosanagnostou@hotmail.com>
2023-12-01 01:31:01 +0100

    Add search parameters to UI types

--- types/UI.ts
+++ types/UI.ts
@@ -10,4 +10,14 @@ export type WithSource = {
     src: string;
 };
 
+export type WithSearchParams = {
+    searchParams?: string | string[] | undefined;
+};
+
+export type WithCategorySearchParams = {
+    searchParams?: {
+        category: string;
+    };
+};
+
 export type AnimationDirection = 'up' | 'down' | 'left' | 'right' | 'default';
