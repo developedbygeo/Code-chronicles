035401aaae517ee62497d5a6b77168bcf8065929
developedbygeo <georgiosanagnostou@hotmail.com>
2023-12-10 01:39:28 +0100

    Remove unused components from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '@/app/styles/globals.css';
 
-import CustomThemeProvider from '@/modules/CustomThemeProvider';
-
-import Navbar from '@/components/modules/Navbar';
-import Footer from '@/components/modules/Footer';
 import { cn } from '@/lib/utils';
 import { WithChildren } from '@/types/UI';
 import { Root } from 'postcss';
@@ -21,13 +17,6 @@ const RootLayout = ({ children }: WithChildren) => (
     <html lang="en" suppressHydrationWarning>
         <body className={cn(inter.className, 'dark:bg-dark bg-gray-100')}>
             <main>{children}</main>
-            {/* <CustomThemeProvider>
-                    <div className="container min-h-screen dark:bg-dark">
-                        <Navbar />
-                        <main>{children}</main>
-                        <Footer />
-                    </div>
-                </CustomThemeProvider> */}
         </body>
     </html>
 );
